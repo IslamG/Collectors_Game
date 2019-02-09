8f5511b4abf33fba17a43e631cf618650f13408c
IslamG <IslamG@github.com>
2019-02-09 18:32:08 +0200

    db now useable anywhere, just include in your code

--- dbconfig.php
+++ dbconfig.php
@@ -8,11 +8,9 @@ $conn=mysqli_connect($host, $user, $pass, $database);
 if(mysqli_connect_error()){
     die("cannot connect to database");
 }
-else
-echo 'database is connected';
-?>
+/*else
+    echo 'database is connected';*/ //I removed the echo so it does not write on page
 
-<?php
-mysqli_close($conn);
+return $conn;
 
 ?>
